65708467da284467a4bffa37b167de19b557940f
João D. Ferreira <jotomicron@gmail.com>
2019-02-03 00:35:39 +0000

    Changed the name of the package
    The name was jdferreira-safeparser and now it is safe-parser.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='jdferreira-safeparser',
+    name='safe-parser',
     version='0.1.1',
     author='João D. Ferreira',
     author_email='jotomicron@gmail.com',
     description='A simple parser of python-line code that can be safely executed',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/jdferreira/simpleparser',
+    url='https://github.com/jdferreira/safe-parser',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
